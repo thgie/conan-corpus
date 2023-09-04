ced1d8e91ddc5475de38a1dd0c8635804dd95231
Adrian <y@thgie.ch>
2023-09-04 15:09:36 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# conan-corpus
+# Conan: The Corpus
+This corpus was made to assist the visual analysis of Conan, a character created by Robert E. Howard. The focus is on the visuality of digital games from the 80ies. What unites them is their visual reference to Conan. This inquiry was initiated via another analysis of the digital game Ball Raider (1987), whose start screen features a reference to He-Man from Masters of the Universe, a franchise inspired by Conan.
+
+## Methodology
+- Design rhetoric on the [visual material](#visual-material)
+- [Corpus analysis](#corpus-analysis) of original Conan stories
+
+## Games
+The following games were analysed through this corpus and inquiry:
+
+- Barbarian: The Ultimate Warrior (1987)
+- Barbarian II: The Dungeon of Drax (1988)
+- Dragonslayer (1988, unreleased)
+- Torvak the Warrior (1990)
+- Conan the Cimmerian (1991)
+
+## Visual material
+An overview of the covers of all media can be browsed under [https://omeka.unibe.ch/s/procedural-visuality/item-set/9186](https://omeka.unibe.ch/s/procedural-visuality/item-set/9186?sort=dcterms%3Acreated+asc). Besides this collection, the visual material includes:
+
+- Covers of the original publications of the stories such as magazines, books and comics
+- Film posters of the Conan movies
+- Covers, promos and screenshots of the digital games packaging
+
+## Corpus analysis
+In order to backtrace the visuality to the original narrative, 17 of the original stories by Robert E. Howard were analysed for visual descriptions of Conan. The python script `main.py` uses spaCy to walk through the text, search for `Conan` and then extracts the sentence's adjectives. Not all are related to visuality, or to Conan, and not all instances of Conan can be captured liked this. Sentences that refer to Conan through `he is/was` are left out, for example. The present approach still delivers enough material to get a rough idea of how Conan was visually imagined.
+
+The cleaning of the text could be improved, `black-haired` is split into `black` and `haired` for example.
---
+++ main.py
@@ -0,0 +1,68 @@
+import json, os, pathlib, spacy
+
+# clean extract file
+if os.path.exists('conan_adj.txt'):
+    os.remove('conan_adj.txt')
+
+adjs = {}
+
+# go through all the files in data folder
+texts = sorted(os.listdir("the_conan_stories"))
+for counter, file in enumerate(texts):
+
+    print("Working on " + str(counter+1) + " of " + str(len(texts)) + ":")
+    print(file+"\n")
+
+    # check if it is a text file, just to make sure
+    if file.endswith(".txt"):
+
+        # load text file and remove new lines; we don't need those
+        text = pathlib.Path(os.path.join("the_conan_stories", file)).read_text(encoding="utf-8")
+        text = text.replace('-\n\n', '-').replace('\n\n', ' ').replace('  ', ' ')
+        text = text.strip()
+
+        # load the transformer model
+        nlp = spacy.load("en_core_web_trf")
+
+        # get all sentences in which Conan and other males are present
+        phrase_matcher = spacy.matcher.PhraseMatcher(nlp.vocab)
+        phrases = ['Conan']
+        patterns = [nlp(text) for text in phrases]
+        phrase_matcher.add('Conan', None, *patterns)
+
+        # shredder that text
+        doc = nlp(text)
+
+        # go through all the sentences of the text
+        for sent in doc.sents:
+
+            # simple workaround to evade duplicates in the extract
+            matched = False
+
+            for match_id, start, end in phrase_matcher(nlp(sent.text)):
+
+                if(matched):
+                    continue
+
+                # check if Conan or other males are present
+                if nlp.vocab.strings[match_id] in ["Conan"]:
+
+                    matched = True
+                    
+                    # check if sentence contains adjectives
+                    pos_types = [token.pos_ for token in sent]
+                    if "ADJ" in pos_types:
+                        for token in sent:
+                            if token.pos_ == "ADJ":
+                                print(token.lemma_)
+                                if token.lemma_ not in adjs:
+                                    adjs[token.lemma_] = []
+                                adjs[token.lemma_].append(sent.text)
+        
+# sort those adjectives
+sorted_adjs = dict(sorted(adjs.items()))
+
+# write those adjectives
+json_adjs = json.dumps(sorted_adjs, indent=4)
+with open("conan_adj.txt", "w") as outfile:
+    outfile.write(json_adjs)
